aeb8b0991a7557edd768e0a85545db1ff4ca0fdb
Turbo <thinhturbo@gmail.com>
2017-06-02 15:43:29 +0200

    Added dd test to Posts controller to test git push

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -17,7 +17,7 @@ class PostsController extends Controller
 {
     public function index()
     {
-        dd("post test");
+        dd("Success");
         $posts = Post::latestFirst()->get();
 
         return fractal()
